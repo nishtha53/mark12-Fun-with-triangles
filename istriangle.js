dd66268ce69e4842b928770e5e935418a99226a3
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-08-18 23:00:41 +0530

    feat Triangle quiz created

---
+++ istriangle.js
@@ -0,0 +1,21 @@
+const  inputs = document.querySelectorAll('.angle-input');
+const buttonTriangle = document.querySelector("#is-triangle-button");
+const outputEL = document.querySelector("#output");
+
+
+function calculateSumOfTraingles(angle1 , angle2 , angle3){
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle(){
+    const sumOfAngles = calculateSumOfTraingles(Number(inputs[0].value) , Number(inputs[1].value) , Number(inputs[2].value));
+    if(sumOfAngles === 180){
+        outputEL.innerText = "Yayy!! The angles form a triangle";
+    }
+    else{
+        outputEL.innerText = "Oh No!! The angles don't  form a triangle";
+    }
+}
+
+buttonTriangle.addEventListener("click", isTriangle)
---
+++ quiz.js
@@ -0,0 +1,34 @@
+const quizForm = document.querySelector('.quiz-form');
+const submitAnswer = document.querySelector('#submit-answer-btn');
+const outputScore = document.querySelector('#output');
+
+const correctAnswer = [
+    "90°",
+    "Right angled",
+    "Polygon",
+    "3",
+    "Equilateral",
+    "A geometric figure",
+    "2",
+    "3",
+    "Isosceles",
+    "Hypotenuse"
+];
+
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    console.log(formResults);
+    for(let value of formResults.values()){
+        // console.log(value);
+        if(value === correctAnswer[index]){
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    outputScore.innerText = "Your Score is " + score;
+}
+
+
+submitAnswer.addEventListener('click',calculateScore);
